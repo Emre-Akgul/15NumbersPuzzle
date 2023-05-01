fdb9ace23228e9ca5bd7d1b8050a1d264ae69ffc
EmreAkgul <emreakgulcs@gmail.com>
2023-05-02 02:30:01 +0300

    solver improved
    solver improved

--- Board.java
+++ Board.java
@@ -1,19 +1,63 @@
-public class Board {
+import java.util.LinkedList;
+
+public class Board implements Comparable<Board>{
     
-    int[][] puzzle;
+    int[] puzzle;
+    int n;
+    int zeroLoc;
+    int numberOfMoves;
+    int manhattanDistance;
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
-    public Board(int[][] tiles){
-        puzzle = tiles;
+    public Board(int[][] tiles, int zeroLoc, int numberOfMoves){
+        n = tiles.length;
+        puzzle = new int[n*n];
+        for(int i = 0; i < n * n; i++){
+            puzzle[i] = tiles[i / n][i % n];
+        }
+        this.zeroLoc = zeroLoc;
+        this.numberOfMoves = numberOfMoves;
+        this.manhattanDistance = manhattan();
+    }
+
+    public Board(Board board, char command){
+        n = board.getSize();
+        if(command == 'l'){
+            this.puzzle = swap(board.puzzle, board.zeroLoc, board.zeroLoc - 1);
+            this.zeroLoc = board.zeroLoc - 1;
+        }else if(command == 'r'){
+            this.puzzle = swap(board.puzzle, board.zeroLoc, board.zeroLoc + 1);
+            this.zeroLoc = board.zeroLoc + 1;
+        }else if(command == 'u'){
+            this.puzzle = swap(board.puzzle, board.zeroLoc, board.zeroLoc - 4);
+            this.zeroLoc = board.zeroLoc - 4;
+        }else{
+            this.puzzle = swap(board.puzzle, board.zeroLoc, board.zeroLoc + 4);
+            this.zeroLoc = board.zeroLoc + 4;
+        }
+        this.numberOfMoves = board.numberOfMoves + 1;
+
+    }
+
+    public int[] swap(int[] puzzle, int index1, int index2){
+        int[] newPuzzle = new int[4 * 4];
+        for(int i = 0; i < n * n; i++){
+            newPuzzle[i] = puzzle[i]; 
+        }
+
+        int temp = newPuzzle[index1];
+        newPuzzle[index1] = newPuzzle[index2];
+        newPuzzle[index2] = temp;
+        return newPuzzle;
     }
 
     // string representation of this board
     public String toString(){
-        String result = getSize() + "\n";
+        String result = getSize() + " - " + numberOfMoves + "\n";
         for(int i = 0; i < getSize() ; i++){
             for(int j = 0; j < getSize(); j++){
-                result = result + puzzle[i][j];
+                result = result + puzzle[i * n + j] + "-";
             }
             result = result + "\n";
         }
@@ -22,12 +66,12 @@ public class Board {
 
     // tile at (row, col) or 0 if blank
     public int tileAt(int col, int row){
-        return puzzle[col][row];
+        return puzzle[col * n + row];
     }
 
     // board size n
     public int getSize(){
-        return puzzle.length;
+        return n;
     }
 
     // number of tiles out of place
@@ -35,7 +79,7 @@ public class Board {
         int res = 0;
         for(int i = 0; i < getSize(); i++){
             for(int j = 0; j < getSize(); j++){
-                if(i * getSize() + j == tileAt(i, j) - 1){
+                if(i * getSize() + j != tileAt(i, j) - 1){
                     res++;
                 }
             }
@@ -47,12 +91,13 @@ public class Board {
     public int manhattan(){
         int res = 0;
         int num;
+        int coef = 1;
         for(int i = 0; i < getSize(); i++){
             for(int j = 0; j < getSize(); j++){
                 
                 num = tileAt(i, j) - 1;
                 if(num >= 0){
-                    res += Math.abs(num / getSize() - i) + Math.abs(num % getSize() - j);
+                    res += coef * (Math.abs(num / getSize() - i) + Math.abs(num % getSize() - j));
                 }
                 
             }
@@ -62,7 +107,7 @@ public class Board {
 
     // is this board the goal board?
     public boolean isGoal(){
-        return hamming() == 15;
+        return hamming() == 0;
     }
 
     // does this board equal y?
@@ -70,7 +115,7 @@ public class Board {
 
         for(int i = 0; i < getSize(); i++){
             for(int j = 0; j < getSize(); j++){
-                if(other.puzzle[i][j] != this.puzzle[i][j]){
+                if(other.puzzle[i * n + j] != this.puzzle[i * n + j]){
                     return false;
                 }
             }
@@ -80,7 +125,45 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors(){
-        return null;
+        LinkedList<Board> neighbors = new LinkedList<>();
+    
+        if(zeroLoc / getSize() > 0 && zeroLoc / getSize() < getSize() - 1 && zeroLoc % 4 > 0 && zeroLoc % 4 < getSize() - 1){
+            neighbors.add(new Board(this,'l'));
+            neighbors.add(new Board(this,'r'));
+            neighbors.add(new Board(this,'u'));
+            neighbors.add(new Board(this,'d'));
+        }else if(zeroLoc / getSize() == 0 && zeroLoc % 4 == 0){
+            neighbors.add(new Board(this,'r'));
+            neighbors.add(new Board(this,'d'));
+        }else if(zeroLoc / getSize() == 0 && zeroLoc % 4 == getSize() - 1){
+            neighbors.add(new Board(this,'l'));
+            neighbors.add(new Board(this,'d'));
+        }else if(zeroLoc / getSize() == getSize() - 1 && zeroLoc % 4 == 0){
+            neighbors.add(new Board(this,'r'));
+            neighbors.add(new Board(this,'u'));
+        }else if(zeroLoc / getSize() == getSize() - 1 && zeroLoc % 4 == getSize() - 1){
+            neighbors.add(new Board(this,'l'));
+            neighbors.add(new Board(this,'u'));
+        }else if(zeroLoc / getSize() == 0){
+            neighbors.add(new Board(this,'l'));
+            neighbors.add(new Board(this,'r'));
+            neighbors.add(new Board(this,'d'));
+        }else if(zeroLoc / getSize() == getSize() - 1 ){
+            neighbors.add(new Board(this,'l'));
+            neighbors.add(new Board(this,'r'));
+            neighbors.add(new Board(this,'u'));
+        }else if(zeroLoc % 4 == 0){
+            neighbors.add(new Board(this,'r'));
+            neighbors.add(new Board(this,'u'));
+            neighbors.add(new Board(this,'d'));
+        }else if(zeroLoc % 4 == getSize() - 1){
+            neighbors.add(new Board(this,'l'));
+            neighbors.add(new Board(this,'u'));
+            neighbors.add(new Board(this,'d'));
+        }
+
+        
+        return neighbors;
     }
 
     // is this board solvable?
@@ -88,15 +171,27 @@ public class Board {
         return false;
     }   
 
-    // unit testing (required)
-    public static void main(String[] args){
+    @Override
+    public int compareTo(Board o) {
+        
+        return this.manhattan() + numberOfMoves - o.manhattan() - o.numberOfMoves;
+
+    }
+
+     // unit testing (required)
+     public static void main(String[] args){
         int[][] solvable2 = {{13,2,10,3},{1,12,8,4},{5,9,6,7},{15,14,11,0}};
-        Board board = new Board(solvable2);
+        Board board = new Board(solvable2,15,0);
         System.out.println(board.hamming());
         System.out.println(board.manhattan());
-        int[][] puzzle2 = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
-        Board board2 = new Board(puzzle2);
+        int[][] puzzle2 = {{1,2,3,4},{5,6,7,8},{9,10,0,12},{13,14,15,11}};
+        Board board2 = new Board(puzzle2,10,0);
         System.out.println(board2.hamming());
         System.out.println(board2.manhattan());
+        System.out.println(board2);
+        Iterable<Board> iter = board2.neighbors();
+        for(Board b : iter){
+            System.out.println(b);
+        }
     }
 }
--- Solver.java
+++ Solver.java
@@ -1,10 +1,69 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 public class Solver {
-    int[][] puzzle;
-
-    public Solver(int[][] puzzle){
-        puzzle = this.puzzle;
+    Board board;
+    TreeMap<Board,Boolean> explored;
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board board){
+        this.board = board; 
+        explored = new TreeMap<>();
     }
+
+    // min number of moves to solve initial board
+    public int moves(){
+        return 0 ;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution(){
+        LinkedList<Board> solution = new LinkedList<Board>();
+        PriorityQueue<Board> pq = new PriorityQueue<>();
+        pq.add(board);
+        explored.put(board, true);
+
+        boolean isReached = false;
+        Board current;
+
+        while(!pq.isEmpty() && !isReached){
+            current = pq.poll();
+            if(current.numberOfMoves > 19){
+                System.out.println(current);
+            }                
+
+            if(current.isGoal()){
+                isReached  = true;
+            }else{
+                Iterable<Board> neighbors = current.neighbors();
+                for(Board b : neighbors){
+                    if(!explored.containsKey(b)){
+                        explored.put(b, true);
+                        pq.add(b);
+                    }
+                }
+            }
+        }
+        System.out.println(isReached);
+        System.out.println("Solved");
+        return solution;
+
+    }
+
+    // test client (see below) 
+    public static void main(String[] args){
+
+        int[][] puzzle2 = {{1,2,3,4},{5,6,7,8},{11,10,9,12},{15,14,13,0}};
+        Board board2 = new Board(puzzle2,15,0);
+        Solver solver = new Solver(board2);
+
+        Iterable<Board> solution = solver.solution();
+        for(Board b : solution){
+            System.out.println(b);
+        }
+    }
+
 
 
 }
